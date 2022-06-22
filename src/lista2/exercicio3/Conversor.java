f2ba6ff3d9b64f159d6820414669666ad529a0bf
p3rsidh <persidh.pimenta@gmail.com>
2022-06-22 12:33:12 -0300

    Criando novo pack para o exercicio 3, o Conversor Criando algoritimo que execute a conversão.

---
+++ src/lista2/exercicio3/Conversor.java
@@ -0,0 +1,17 @@
+package src.lista2.exercicio3;
+
+import java.util.Scanner;
+
+public class Conversor {
+    public static void main(String[] args) {
+        float Fh;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Aqui voce pode converter Fahrenheit em Celsius");
+        System.out.println("Insira o valor da temperatura que deseja converter em Celsius (Insira em Fahrenheit");
+        Fh = input.nextFloat();
+        double Cl =((Fh - 32)/(1.8));
+        System.out.println("Convertendo...");
+        System.out.printf( +Fh +" Fahrenheit  equivalem a " +Cl +" Celsius");
+    }
+}
