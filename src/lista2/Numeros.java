e7232589cd5b0ab13307792dd26c9b56a9513b29
p3rsidh <persidh.pimenta@gmail.com>
2022-06-22 13:06:51 -0300

    Criando algoritimo para apresentar o antecessor e sucessor de um numero

---
+++ src/lista2/Numeros.java
@@ -0,0 +1,9 @@
+package src.lista2;
+
+public class Numeros {
+    public static void main(String[] args) {
+        System.out.println("Aqui voce pode descobrir o antecessor e sucessor de um numero");
+        System.out.println("Digite o numero que deseja");
+
+    }
+}
