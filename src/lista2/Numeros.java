f30b8f5f6d3eddbdcc88649b29da15418e300771
p3rsidh <persidh.pimenta@gmail.com>
2022-06-22 13:10:09 -0300

    Recebendo um numero e apresentando seu antecessor e sucessor

--- src/lista2/Numeros.java
+++ src/lista2/Numeros.java
@@ -1,9 +1,16 @@
 package src.lista2;
 
+import java.util.Scanner;
+
 public class Numeros {
     public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Aqui voce pode descobrir o antecessor e sucessor de um numero");
         System.out.println("Digite o numero que deseja");
+        n = input.nextInt();
+        System.out.println("O numero " +n +"Tem como antecessor " +(n-1) +" e como sucessor " +(n+1));
 
     }
 }
