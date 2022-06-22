2125f36b5f8724df1e4f3a35b240da99742383ed
p3rsidh <persidh.pimenta@gmail.com>
2022-06-22 11:38:44 -0300

    criando algoritimo que recebe o nome do aluno

--- src/lista2/exercicio2/NotasBim.java
+++ src/lista2/exercicio2/NotasBim.java
@@ -1,4 +1,20 @@
 package src.lista2.exercicio2;
 
+import java.util.Scanner;
+
 public class NotasBim {
+    public static void main(String[] args) {
+        float n1, n2, n3, n4;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o nome do aluno");
+        String nome = input.nextLine();
+        System.out.println("O nome do aluno e: " +nome);
+        System.out.println("Digite a primeira nota do aluno:");
+        n1 = input.nextFloat();
+        System.out.println("A primeira nota e:" +n1);
+        System.out.println("Digite a segunda nota:");
+
+
+    }
 }
