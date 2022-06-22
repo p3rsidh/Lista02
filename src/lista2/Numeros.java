49e09ee0558be7ca524a3a50cc3419c6cdbc06dd
p3rsidh <persidh.pimenta@gmail.com>
2022-06-22 13:11:01 -0300

    Corrigindo sequencia de escrita.

--- src/lista2/Numeros.java
+++ src/lista2/Numeros.java
@@ -10,7 +10,7 @@ public class Numeros {
         System.out.println("Aqui voce pode descobrir o antecessor e sucessor de um numero");
         System.out.println("Digite o numero que deseja");
         n = input.nextInt();
-        System.out.println("O numero " +n +"Tem como antecessor " +(n-1) +" e como sucessor " +(n+1));
+        System.out.println("O numero " +n +" tem como antecessor " +(n-1) +" e como sucessor " +(n+1));
 
     }
 }
